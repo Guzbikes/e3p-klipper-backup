80710c160a25d60c107bcc8765d52c8abf4aa861
guzbikes <ryanguzy@gmail.com>
2026-03-01 05:40:40 -0700

    maintenance_mode.sh modified - 03/01/2026 - 05:40:37 AM

---
+++ printer_data/config/maintenance_mode.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# List of timers you identified as risky
+TIMERS="fstrim.timer dpkg-db-backup.timer systemd-tmpfiles-clean.timer"
+
+if [ "$1" == "off" ]; then
+    echo "Disabling background tasks for printing..."
+    sudo systemctl stop cron
+    sudo systemctl mask $TIMERS
+elif [ "$1" == "on" ]; then
+    echo "Enabling background tasks..."
+    sudo systemctl unmask $TIMERS
+    sudo systemctl start cron
+    # Manually trigger a start for timers just in case they were missed
+    sudo systemctl start fstrim.timer
+else
+    echo "Usage: $0 {on|off}"
+fi
